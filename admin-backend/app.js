21cc5c98cf9a614ab23d4cbce6229de35f968a2e
William Omø <williamjean.k.o@gmail.com>
2023-03-08 09:44:46 +0100

    setup for easy use with non init units

--- admin-backend/app.js
+++ admin-backend/app.js
@@ -1,9 +1,13 @@
 import express from "express";
-import userRouter from "routers/user-router.js";
+import userRouter from "./routers/user-router.js";
 
 const app = express();
 
 app.use(userRouter);
+
+app.get("/", (req, res) =>{
+    res.send("Welcome to admin frontpage!")
+})
 
 const PORT = 3000;
 app.listen(PORT, () => console.log("Server running on:", PORT));
--- database-manager/app.js
+++ database-manager/app.js
@@ -1,9 +1,12 @@
 import express from "express";
-import userRouter from "./routers/user-router.js";
+//import userRouter from "./routers/user-router.js";
 
 const app = express();
 
-app.use(userRouter);
+//app.use(userRouter);
 
+app.get("/", (req, res) => {
+    res.send("Welcome to db manager frontpage!")
+})
 const PORT = 5050;
 app.listen(PORT, () => console.log("Server running on:", PORT));
